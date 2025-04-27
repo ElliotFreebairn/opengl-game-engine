2661c32bc89b4bd9065bab0bae28d6c8df6bbd09
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-04-27 23:07:34 +0100

    More open gl practice. glfwWindow is working and fixed the make file which had some issues with linking and compiling

--- OpenGLPractice/main.cpp
+++ OpenGLPractice/main.cpp
@@ -3,13 +3,40 @@
 
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main()
 {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Game Engine Practice", NULL, NULL);
+  if(window == NULL) {
+    std::cout << "Failed to create GLFW Window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   
-  std::cout << "Loaded glfw" << std::endl;
+  if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
