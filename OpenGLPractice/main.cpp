45d2144e8e09933a0c3459115c70912e6f6b21d2
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-05-02 00:34:13 +0100

    Created first vertex buffer object with vertex data for a triangle, will now move on to how shaders work

--- OpenGLPractice/main.cpp
+++ OpenGLPractice/main.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
@@ -27,6 +30,24 @@ int main()
   }
 
   glViewport(0, 0, 800, 600);
+
+  float vertices[] = { // Y-axis points up in the up-direction and (0,0) is the center
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f,
+  };
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  /* Copies vertices into bound buffer. args[1] = specifies size of data in bytes to pass,
+   * args[2] = the actual data to send, args[2] = How we want the graphics card to manage data
+   * GL_STATIC_DRAW = the data is set only once and used many times, can use DYNAMIC_DRAW 
+   * for varying data (allows for faster writes)*/
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
 
   while(!glfwWindowShouldClose(window)) {
     glfwSwapBuffers(window);
