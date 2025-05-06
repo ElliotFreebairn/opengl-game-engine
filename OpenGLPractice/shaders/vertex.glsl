bedfa783a414494f4830c3a9d971dc7b72e6a55a
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-05-06 23:34:48 +0100

    The colour now is interpolated. This is due to adding colour values inside vertices data then re-adjusting vertex attribute pointers to adjust the stride and offset

--- OpenGLPractice/shaders/vertex.glsl
+++ OpenGLPractice/shaders/vertex.glsl
@@ -1,10 +1,11 @@
 #version 330 core
 layout (location = 0) in vec3 aPos; // position has attribute position 0
+layout (location = 0) in vec3 aColor;
 
-out vec4 vertexColor; // specify a color output to the fragment shader
+out vec3 ourColor;
 
 void main()
 {
   gl_Position = vec4(aPos, 1.0); // we give a vec3 to vec4’s constructor
-  vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // output variable to dark-red
+  ourColor = aColor;
 }
