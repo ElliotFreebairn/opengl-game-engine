84dc7cd1b144edbbe97fd07344fe6011faacd46d
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-05-09 18:19:51 +0100

    Fixed the segmentation fault. It was caused by using a int instead of a byte, now the texture is fully working

--- OpenGLPractice/main.cpp
+++ OpenGLPractice/main.cpp
@@ -71,27 +71,23 @@ int main()
   glGenBuffers(1, &VBO);
   glGenBuffers(1, &EBO);
 
-  /* 1. bind VAO */
   glBindVertexArray(VAO);
-  /* 2. copy vertices array in buffer for OpenGL to use */
+
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-  /* 3. then set vertex attributes pointers */
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void*)0); 
+
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  /* Color attribute */
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void*)(3 * sizeof(float)));
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
-  /* 4. Texture coords attribute poitners */
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void*)(6 * sizeof(float)));
+  // texture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
   glEnableVertexAttribArray(2);
-
-
   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
   
  /* ------------ Textures ----------------- */
@@ -110,7 +106,7 @@ int main()
   unsigned char *data = stbi_load("resources/textures/container.jpg", &width, &height,
                                   &nrChannels, 0);
   if(data) {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_INT, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
   } else {
     std::cout << "Failed to load texture" << std::endl;
