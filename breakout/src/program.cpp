0cb7c4232f5345ed3cd9e7d1a10f5b341d95138c
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-06-10 22:56:49 +0100

    Added the skeleton code needed game object + a little of the window creation in program.cpp

--- breakout/src/program.cpp
+++ breakout/src/program.cpp
@@ -1,11 +1,48 @@
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <GLFW3/glfw3.h>
 
 #include "game.h"
 #include "resource_manager.h"
 
-int main() 
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+Game Breakout(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+int main(int argc, char *argv[])
 {
-  
-  return 0;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, false);
+
+  GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout", nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+
+  // glad: load all OpenGL function pointers
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initalize GLAD" << std::endl;
+    return -1;
+  }
+
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetFrameBufferSizeCallback(window, framebuffer_size_callback);
+
+  // OpenGL configuration
+  // --------------------
+  glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  // initalize game
+  // -------------
+  Breakout.Init();
 }
+
