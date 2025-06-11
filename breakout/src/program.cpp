fa999d673e3466324c69c56b4e11aec3c56e01a5
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-06-11 22:17:47 +0100

    A blank window is now opening on screen. Added all basic resource mangament/window skeleton code. Now start working on camera set up/levels

--- breakout/src/program.cpp
+++ breakout/src/program.cpp
@@ -77,4 +77,23 @@ int main(int argc, char *argv[])
   return 0;
 }
 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) 
+{
+  // when a user presses the escape key, close window
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+  if (key >= 0 && key < 1024)
+  {
+    if (action == GLFW_PRESS)
+      Breakout.Keys[key] = true;
+    else if (action == GLFW_RELEASE)
+      Breakout.Keys[key] = false;
+  }
+}
+
+void framebuffer_size_callback(GLFWwindow* wndow, int width, int height)
+{
+  // make sure viewport matches the new window dimensions
+  glViewport(0, 0, width, height);
+}
 
