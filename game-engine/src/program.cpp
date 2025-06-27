a73c244334f76c58a459d6aa5922c8dfe1dcedfc
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-06-27 22:49:37 +0100

    Basic program code to get glad and glfw to work. This will be the start of my game engine from scratch with no tutorial. Will be using knowledge learnt from making breakout and the tutorials from learningopengl.com

---
+++ game-engine/src/program.cpp
@@ -0,0 +1,47 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+int main()
+{
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // use opengl core, access subset of features
+  
+  GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game Engine", NULL, NULL);
+  if (window == NULL)
+  {
+    std:: cout << "GLFW Window failed to create" << std::endl;
+    glfwTerminate(); // destorys remaining windows and cursors
+  }
+  glfwMakeContextCurrent(window);
+
+  /* Initialize GLAD - deals  with function pointers for opengl */
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "GLAD failed to initialise" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window); // swaps the colour buffer, used to render and show output
+    glfwPollEvents(); // checks if any events are triggered (keyboard or mousemovement), calls functions via callback methods
+  }
+
+
+  return 0;
+}
+
+// resizes viewport when user resizes the glfw window
+void frame_buffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
