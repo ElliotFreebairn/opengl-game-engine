1a83b29a436568a0443b2f0a9f6b57061666f1fd
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-10-26 22:35:36 +0000

    Removing vscode from git + set up for scene functionality
    Signed-off-by: elliotfreebairn <elliot.freebairn@collabora.com>

---
+++ game-engine/src/scene.h
@@ -0,0 +1,18 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+/*
+    What's the purpose of the scene?
+        - it store all the objects in a scene/(game)
+        - draw all the objects in the scene/game
+        - initialise the data from scratch
+        - load in scenes/levels if so
+*/
+class Scene
+{
+public:
+
+private:
+
+}
+#endif
