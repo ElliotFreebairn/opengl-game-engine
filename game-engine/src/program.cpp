be7043be6fbe7f512a29dba9beeab9a796a6bdca
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-11-09 00:35:40 +0000

    Add light blue background
    Signed-off-by: elliotfreebairn

--- game-engine/src/program.cpp
+++ game-engine/src/program.cpp
@@ -93,7 +93,7 @@ int main()
         game.Update(deltaTime);
         ui_manager.Update(window, deltaTime, lastX, lastY, game);
 
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.5f, 0.9f, 0.95f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		game.Render();
